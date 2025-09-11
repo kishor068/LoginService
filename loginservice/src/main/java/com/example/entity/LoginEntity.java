1a3f9ac2c7bf3daa93366ff3ffab8cb37c69e52d
deepthi20kulal <20deepthikulal@gmail.com>
2025-09-11 20:01:19 +0530

    Added validations for the entity

--- loginservice/src/main/java/com/example/entity/LoginEntity.java
+++ loginservice/src/main/java/com/example/entity/LoginEntity.java
@@ -1,6 +1,9 @@
 package com.example.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,9 +17,19 @@ public class LoginEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @NotNull(message = "Id cannot be null")
     private Long id;
     @Column
+    @NotBlank(message = "Name cannot be Blank")
     private String username;
     @Column
+    @NotBlank(message = "please enter the password")
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&+=])(?=\\S+$).{8,20}$",
+            message = "Password must contain at least one digit, " +
+                    "one lowercase letter, " +
+                    "one uppercase letter, " +
+                    "one special character, " +
+                    "no whitespace, " +
+                    "and be 8-20 characters long.")
     private String password;
 }
