0387b9a9727aee1635ca2694c37379981004b953
Preethika Shet <143956104+preethikashet@users.noreply.github.com>
2025-09-23 09:47:11 +0530

    added slf4j in controller, pom.xml and properties

--- loginservice/src/main/java/com/example/controller/LoginController.java
+++ loginservice/src/main/java/com/example/controller/LoginController.java
@@ -1,8 +1,12 @@
 package com.example.controller;
 
+
 import com.example.dao.LoginRepository;
 import com.example.entity.LoginEntity;
 import com.example.service.LoginService;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
@@ -14,7 +18,11 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/users")
 @CrossOrigin(origins="http://localhost:3000")
+//@Slf4j //HERE because we add this logger isnt required
+
 public class LoginController {
+
+   static Logger logger = LoggerFactory.getLogger(LoginController.class);
 
 @Autowired
     private LoginRepository loginRepository;
@@ -43,8 +51,11 @@ public class LoginController {
     }
 
     @PostMapping("/adduser")
+
     public ResponseEntity<LoginEntity> createuser(@RequestBody LoginEntity loginEntity)
     {
+       logger.info("Login attempt"); //not working for @slf4j
+//        log.info("login attempt"); //log is only used
         return ResponseEntity.ok(loginService.createUser(loginEntity));
     }
 }
